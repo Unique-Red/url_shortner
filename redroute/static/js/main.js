02948401fecf6af4e18443ecfec7b171b07daefd
unique-red <noah13victor@gmail.com>
2023-05-31 19:40:42 +0100

    added event listener for navbar scroll

--- redroute/static/js/main.js
+++ redroute/static/js/main.js
@@ -1,5 +1,15 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+let navbar = document.querySelector('.nav-el');
+
+window.onscroll = () => {
+    if (window.scrollY > 20 && window.innerWidth > 768) {
+        navbar.classList.add('fixed');
+    }
+    else {
+        navbar.classList.remove('fixed');
+    }
+}
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
