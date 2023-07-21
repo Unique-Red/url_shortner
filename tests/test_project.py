0066f43d20dedf9359f563137521892dda811a03
unique-red <noah13victor@gmail.com>
2023-07-21 21:34:08 +0100

    test

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,13 +1,14 @@
 from redroute import app, db
 from redroute.models import User, Url
 
-from redroute import app
-
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
 
-def test_login():
-    response = app.test_client().get('/login')
-    
-    assert response.status_code == 200
+def test_register():
+    response = app.test_client().post('/register', data={'username': 'test', 'email': 'test@gmail.com', 'password': 'test', 'confirm_password': 'test'})
+
+    with app.app_context():
+        assert User.query.count() == 1
+        assert User.query.first().username == 'test'
+
