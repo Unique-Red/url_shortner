0cc9dae8e0700433957e7c78344f08b5fd224af3
unique-red <noah13victor@gmail.com>
2023-09-10 15:47:24 +0100

    error pages

--- redroute/routes.py
+++ redroute/routes.py
@@ -275,3 +275,16 @@ def stats():
     clicks = Url.query.with_entities(func.sum(Url.clicks)).scalar()
 
     return render_template('stats.html', users=users, links=links, clicks=clicks)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return render_template('429.html'), 429
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
